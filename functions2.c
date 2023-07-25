76250c37f37ccf5fe8fb3467d6a25f6d4395837f
Boudy560 <body809070@gmail.com>
2023-07-25 16:01:13 -0700

    all

---
+++ functions2.c
@@ -0,0 +1,18 @@
+
+			if (in[j] == str[i])
+			{
+				x = out[j];
+				write(1, &x, 1);
+				count++;
+				break;
+			}
+		}
+		if (!in[j])
+		{
+			x = str[i];
+			write(1, &x, 1);
+			count++;
+		}
+	}
+	return (count);
+}
